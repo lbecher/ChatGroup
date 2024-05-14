172b6b38c2f9b83e7a12a24d294d2f18b6030b8c
EduardoGarzon <luizeduardo3131@gmail.com>
2024-05-14 09:18:34 -0300

    verificacao de nomes cadastrados.
    Implementacao da verificacao de nomes de usuarios registrados no servidor.

--- src/main/java/br/unioeste/Server.java
+++ src/main/java/br/unioeste/Server.java
@@ -5,28 +5,57 @@ import java.net.*;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Server {
-    private final int port;
+    private final int port; // Numero da porta do servidor.
+
+    // Lista de acesso concorrente para threads ClientHandler.
     private static final CopyOnWriteArrayList<ClientHandler> clients = new CopyOnWriteArrayList<>();
 
     public Server(String port) {
         this.port = Integer.parseInt(port);
     }
 
+    private void serverMessage(String str) {
+        System.out.println("[Server Message] : " + str);
+    }
+
+    // Metodo que verifica se o nome de usuario ja esta sendo utilizado.
+    private boolean isUsernameTaken(String username) throws IOException {
+        for (ClientHandler clientHandler : clients) {
+            if (clientHandler.getUsername().equals(username)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Método que inicia o servidor.
     public void run() {
-        System.out.println("Iniciando o servidor...");
+        serverMessage("Inicialiazando o servidor...");
 
         try {
+            // Servidor aguarda por conexoes.
             ServerSocket serverSocket = new ServerSocket(this.port);
-            System.out.println("O servidor está rodando e aguardando por conexões.");
+            serverMessage("O servidor está rodando e aguardando por conexões.");
 
             while (true) {
                 Socket clientSocket = serverSocket.accept();
-                System.out.println("Novo cliente conectado: " + clientSocket);
+                serverMessage("Novo cliente conectado: " + clientSocket);
 
                 ClientHandler clientHandler = new ClientHandler(clientSocket);
-                clients.add(clientHandler);
-                new Thread(clientHandler).start();
+                clientHandler.setUsername();    // Configura nome de usuario.
+                boolean usernameTaken = false;  // Flag para verificar se o nome ja foi utilizado.
+
+                usernameTaken = isUsernameTaken(clientHandler.getUsername());
+
+                // Se nome nao utilizado : add novo usuario na lista e cria nova thread.
+                if (!usernameTaken) {
+                    clients.add(clientHandler);
+                    new Thread(clientHandler).start();
+                } else {
+                    clientSocket.close(); // Fecha a conexão com o cliente.
+                }
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -40,16 +69,19 @@ public class Server {
         }
     }
 
+    // ClientHandler para lidar com os clientes conectados.
     private static class ClientHandler implements Runnable {
         private Socket socket;
         private PrintWriter out;
         private BufferedReader in;
+        private String username;
 
         public ClientHandler(Socket socket) {
             this.socket = socket;
+            this.username = null;
 
             try {
-                // Create input and output streams for communication 
+                // Cria streams de entrada e saida atraves do socket para o cliente.
                 out = new PrintWriter(socket.getOutputStream(), true);
                 in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             } catch (IOException e) {
@@ -60,24 +92,23 @@ public class Server {
         @Override
         public void run() {
             try {
-                String username = getUsername();
-                System.out.println("User " + username + " connected.");
+                System.out.println("User " + getUsername() + " connected.");
 
-                out.println("Welcome to the chat, " + username + "!");
+                out.println("Welcome to the chat, " + getUsername() + "!");
                 out.println("Type Your Message");
                 String inputLine;
 
                 while ((inputLine = in.readLine()) != null) {
-                    System.out.println("[" + username + "]: " + inputLine);
+                    System.out.println("[" + getUsername() + "]: " + inputLine);
 
-                    // Broadcast the message to all clients 
-                    broadcast(this, "[" + username + "]: " + inputLine);
+                    // Mensagem enviada a todos do servidor via broadcast.
+                    broadcast(this, "[" + getUsername() + "]: " + inputLine);
                 }
 
-                // Remove the client handler from the list 
+                // Remove o clienthandler da lista.
                 clients.remove(this);
 
-                // Close the input and output streams and the client socket 
+                // Fecha streams e sockets do cliente.
                 in.close();
                 out.close();
                 socket.close();
@@ -86,14 +117,20 @@ public class Server {
             }
         }
 
-        private String getUsername() throws IOException {
+        public String setUsername() throws IOException {
             out.println("Enter your username:");
             return in.readLine();
+        }
+
+        public String getUsername() throws IOException {
+            return this.username;
         }
 
         public void sendMessage(String message) {
             out.println(message);
             out.println("Type Your Message");
         }
+
+
     }
 }
