7a47292fcd10ea77d66fc904e8937d7c037886e5
Luiz Fernando Becher de Araujo <luiz.becher.araujo@gmail.com>
2024-05-15 19:23:10 -0300

    Adicionando pontos nos comentários

--- src/main/java/br/unioeste/Main.java
+++ src/main/java/br/unioeste/Main.java
@@ -1,7 +1,5 @@
 package br.unioeste;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         if (args.length < 2) {
