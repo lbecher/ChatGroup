657c6df964eea5c7361d1528dfd64d74407841f4
EduardoGarzon <luizeduardo3131@gmail.com>
2024-05-20 23:22:14 -0300

    Implementacoes.
    Implementado: protocolo de saidas de salas, Listagem indicativa de sala publicas ou privadas e finalizacao do protocolo de entrada em salas.

--- src/main/java/br/unioeste/Server.java
+++ src/main/java/br/unioeste/Server.java
@@ -7,6 +7,7 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
@@ -53,40 +54,47 @@ public class Server {
 
     // Método que verifica se o nome de usuário já esta sendo utilizado.
     private static boolean isUsernameTaken(String username) throws IOException {
-        if (clients.containsKey(username)) {
-            return true;
-        }
-        return false;
+        return clients.containsKey(username);
     }
     
 
+    // --------------------------------------------------------------------------------------------------------------
 
-    // ---------------------------------------------------
-    // Classe para as gerenciar as salas.
+
+    // Classe para gerenciar as salas.
     private static class Room {
         // Nomes de usuários dos membros.
         private HashSet<String> members;
-
         private String room_name;
         private String admin;
         private String hashedPassword;
+        private boolean is_private;
 
+        // Construtor.
         public Room(String room_name, String admin, String hashedPassword) {
             this.members = new HashSet<String>();
-
             this.room_name = room_name;
             this.admin = admin;
             this.hashedPassword = hashedPassword;
+            is_private = hashedPassword != null;
         }
 
+        // Método para verificar se uma sala é privada.
+        public boolean isRoomPrivate() {
+            return is_private;
+        }
+
+        // Método para verificar se um usuário ja é membro da sala.
         public boolean isMember(String username) {
             return this.members.contains(username);
         }
 
+        // Método que adiciona novo membro na sala.
         public void addMember(String username) {
             this.members.add(username);
         }
 
+        // Método que remove um memebro da sala.
         public void removeMember(String username, ClientHandler client) {
             if (this.admin.equals(username)) {
                 client.sendMessage("ERRO O administrador não pode ser removido!");
@@ -95,13 +103,12 @@ public class Server {
             this.members.remove(username);
         }
 
+        // Método que valida a senha hash.
         public boolean validateHashedPassword(String hashedPassword) {
-            if (this.hashedPassword != null && !this.hashedPassword.equals(hashedPassword)) {
-                return false;
-            }
-            return true;
+            return (this.hashedPassword != null && !this.hashedPassword.equals(hashedPassword)) ? false : true;
         }
 
+        // Método que encaminha as mensagens para os membros da sala.
         public void sendMessageForMembers(String sender, String sender_message) {
             String message = "MENSAGEM " + this.room_name + " " + sender + " " + sender_message;
 
@@ -113,19 +120,26 @@ public class Server {
                 client.sendMessage(message);
             }
         }
+
+        // Método que lista todos os membros da sala.
+        public String listAllRoomMembers () {
+            String allMembers = members.stream().collect(Collectors.joining(" "));
+            return "ENTRAR_SALA_OK " + allMembers;
+        }
     }
 
 
-
     // ---------------------------------------------------
+
+
     // Classe para lidar com os clientes conectados.
     private static class ClientHandler implements Runnable {
         private String username;
-        
         private Socket socket;
         private PrintWriter out;
         private BufferedReader in;
 
+        // Construtor.
         public ClientHandler(Socket socket) {
             this.socket = socket;
             this.username = null;
@@ -142,9 +156,9 @@ public class Server {
         @Override
         public void run() {
             try {
-                // Registra soquete e nome de usuário em clients. Permanece 
-                // nesse método até obter um nome de usuário válido ou a
-                // conexão ser fechada.
+                // Registra soquete e nome de usuário em clients.
+                // Permanece nesse método até obter um nome de usuário válido
+                // ou a conexão ser fechada.
                 this.registerClient();
 
                 while (true) {
@@ -168,7 +182,7 @@ public class Server {
                             break;
                         
                         case "SAIR_SALA":
-                            
+                            handleExitRoom(splited_message);
                             break;
                         
                         case "FECHAR_SALA":
@@ -190,6 +204,7 @@ public class Server {
             }
         }
 
+        // Método que registra o usuário no servidor.
         public void registerClient() throws IOException {
             while (true) {
                 // Deve ter o formato REGISTRO <username>.
@@ -222,6 +237,7 @@ public class Server {
             }
         }
 
+        // Método que adiciona um membro na sala.
         private void handleJoinRoom(String[] splited_message) { 
             String room_name = splited_message[1];
 
@@ -233,9 +249,16 @@ public class Server {
             Room room = rooms.get(room_name);
             room.addMember(this.username);
 
-            sendMessage("ENTRAR_SALA_OK");
+            sendMessage(room.listAllRoomMembers());
+
+            ClientHandler admin = clients.get(room.admin);
+            for (String member : room.members) {
+                if (!Objects.equals(member, this.username))
+                    admin.sendMessage("ENTROU " + room.room_name + " " + this.username);
+            }
         }
 
+        // Método que encaminha as mensagens para os membros da sala.
         private void handleSendMessage(String[] splited_message) {
             if (splited_message.length < 3) {
                 sendMessage("ERRO Argumentos faltando em ENVIAR_MENSAGEM!");
@@ -265,8 +288,9 @@ public class Server {
             room.sendMessageForMembers(this.username, message);
         }
 
+        // Método para criar uma sala.
         private void handleRoomCreation(String[] splited_message) {
-            if (splited_message.length != 3) {
+            if (splited_message.length <= 2 || splited_message.length >= 5) {
                 sendMessage("ERRO Argumentos faltando em CRIAR_SALA!");
                 return;
             }
@@ -290,6 +314,7 @@ public class Server {
             }
         }
 
+        // Método que cria as salas publicas ou privadas.
         private void createRoom(String room_name, String admin, String hashedPassword) {
             if (!rooms.containsKey(room_name)) {
                 Room room = new Room(room_name, admin, hashedPassword);
@@ -300,11 +325,51 @@ public class Server {
             }
         }
 
+        // Método para sair de uma sala.
+        private void handleExitRoom(String[] splited_message) {
+            if (splited_message.length != 2) {
+                sendMessage("ERRO Argumentos inválidos para o comando SAIR_SALA!");
+                return;
+            }
+
+            if (!rooms.containsKey(splited_message[1])) {
+                sendMessage("ERRO A sala informada nao existe!");
+                return;
+            }
+
+            if (!rooms.get(splited_message[1]).isMember(this.username)) {
+                sendMessage("ERRO Usuário não é membro da sala.");
+                return;
+            }
+
+            sendMessage("SAIR_SALA_OK");
+
+            ClientHandler admin = clients.get(rooms.get(splited_message[1]).admin);
+            for (String member : rooms.get(splited_message[1]).members) {
+                if (!Objects.equals(member, this.username)) {
+                    admin.sendMessage("SAIU " + rooms.get(splited_message[1]).room_name + " " + this.username);
+                }
+            }
+
+            rooms.get(splited_message[1]).members.remove(this.username);
+
+            if (Objects.equals(rooms.get(splited_message[1]).admin, this.username)) {
+                rooms.remove(splited_message[1]);
+            }
+
+        }
+
+        // Método que lista todas as salas.
         private void listRooms() {
-            String concatenatedRoomsNames = rooms.keySet().stream()
-                .filter(name -> name instanceof String)
-                .map(name -> (String) name)
-                .collect(Collectors.joining(" "));
+            String concatenatedRoomsNames = rooms.entrySet().stream()
+                    .filter(entry -> entry.getKey() instanceof String && entry.getValue() instanceof Room)
+                    .map(entry -> {
+                        String name = entry.getKey();
+                        Room room = entry.getValue();
+                        String availabilityString = room.isRoomPrivate() ? "Privada" : "Publica";
+                        return name + "(" + availabilityString + ")";
+                    })
+                    .collect(Collectors.joining(" "));
             
             sendMessage("SALAS " + concatenatedRoomsNames);
         }
@@ -319,14 +384,17 @@ public class Server {
             socket.close();
         }
 
+        // Método que retorna o nome do usuário.
         public String getUsername() throws IOException {
             return this.username;
         }
 
+        // Método que envia as mensagens digitadas.
         private void sendMessage(String message) {
             out.println(message);
         }
 
+        // Método que capta as mensagens digitadas.
         private String recieveMessage() throws IOException {
             return in.readLine();
         }
